3df9dc68209b56c79a8209cfcb460f1b89928dc1
mokha <mokha@cisco.com>
2018-01-29 18:33:15 -0700

    require openssl

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require "bundler/setup"
 require "scim/shady"
 require "securerandom"
 require "ffaker"
+require "openssl"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
